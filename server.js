2c3f41b8cbb89a62cbc1e0048c4a77b0b3a43529
priyanka10it <priyanka10.it@gmail.com>
2020-11-04 21:51:21 -0700

    MVP met

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
+const helmet = require('helmet');
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
+server.use('/api/posts',postRouter);
+server.use('/api/users',userRouter);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +17,13 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+     `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.originalUrl}
+    }` 
+    //console.log(req.method, "Original Url:", req.originalUrl, new Date() )
+  );
 
+  next();
+}
 module.exports = server;
